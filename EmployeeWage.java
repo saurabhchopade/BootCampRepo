6c562478579edb8b234d1ee5e2ab94b9c7d978ee
saurabh chopade <saurabhchopade3250@gmail.com>
2020-04-28 02:52:41 +0530

    Attendence feature Added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,2 +1,20 @@
-Welcome
+import java.util.Random; 
+  
+	public class EmployeeWage{ 
+  
+		public static void main(String args[]){
+
+      	Random rand = new Random(); 
+        	int randomNum = rand.nextInt(1000); 
+
+			if(randomNum%2==1) {
+				System.out.println("Employee Present");
+		
+			}
+			else{
+
+				System.out.println("Employee Absent");
+			}
+		}
+	}
 
