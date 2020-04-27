76ad0b0842ebe19eb721f3e3af81ee6bee05f950
saurabh chopade <saurabhchopade3250@gmail.com>
2020-04-28 03:40:03 +0530

    total Wage of month Added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,25 +2,29 @@ import java.util.Random;
   
 	public class EmployeeWage{   
 		public static void main(String args[]){
-					
-			int empRateperHour=50,empHour=0,sallary=0;
+
+			int empRateperHour=50,empHour=0,sallary=0,totalWage=0;
 			int fullTime=1,partTime=0;
-      	Random rand = new Random(); 
-        	int randomNum = rand.nextInt(1000)%3; 
-	
-			switch(randomNum){
-			case 0:
-				empHour=8;
-				break;
-			case 1:
-				empHour=4;
-				break;
-			default:
-				empHour=0;
 
+			for(int month=1;month<=20;month++) {
+
+      		Random rand = new Random(); 
+        		int randomNum = rand.nextInt(1000)%3; 	
+				switch(randomNum){
+				case 0:
+					empHour=8;
+					break;
+				case 1:
+					empHour=4;
+					break;
+				default:
+					empHour=0;
+
+				}
+		 	sallary=empHour*empRateperHour;
+			totalWage=totalWage+sallary;
 			}
-		 	System.out.println("Sallry="+empHour*empRateperHour);
+			System.out.println("TotalWage="+totalWage);
 		}
-
 	}
 
