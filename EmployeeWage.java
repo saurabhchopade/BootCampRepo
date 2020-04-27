d0bcd11018e4f74b92d321808a422fbedef733fb
saurabh chopade <saurabhchopade3250@gmail.com>
2020-04-28 03:28:11 +0530

    Switch feature Added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,16 +7,15 @@ import java.util.Random;
 			int fullTime=1,partTime=0;
       	Random rand = new Random(); 
         	int randomNum = rand.nextInt(1000)%3; 
-
-			if(randomNum==1) {
+	
+			switch(randomNum){
+			case 0:
 				empHour=8;
-				
-			}
-			else if(randomNum==0){
+				break;
+			case 1:
 				empHour=4;
-
-			}
-			else {
+				break;
+			default:
 				empHour=0;
 
 			}
