1ddcc266ab1d9368217981a921b6b83e0efc0818
saurabh chopade <saurabhchopade3250@gmail.com>
2020-04-28 09:07:09 +0530

    reached feature

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,10 +3,10 @@ import java.util.Random;
 	public class EmployeeWage{   
 		public static void main(String args[]){
 
-			int empRateperHour=50,empHour=0,sallary=0,totalWage=0;
+			int empRateperHour=50,empHour=0,sallary=0,totalWage=0,totalWorkingHour=0;
 			int fullTime=1,partTime=0;
 
-			for(int month=1;month<=20;month++) {
+			for(int month=1;month<=50;month++) {
 
       		Random rand = new Random(); 
         		int randomNum = rand.nextInt(1000)%3; 	
@@ -23,8 +23,14 @@ import java.util.Random;
 				}
 		 	sallary=empHour*empRateperHour;
 			totalWage=totalWage+sallary;
+			totalWorkingHour=totalWorkingHour+empHour;
+			if(totalWorkingHour==100){
+				System.out.println("Totatreached"+totalWorkingHour);
+				System.out.println("TotalWage="+totalWage);  
+				System.exit(0);
 			}
-			System.out.println("TotalWage="+totalWage);
+
+			}
 		}
 	}
 
